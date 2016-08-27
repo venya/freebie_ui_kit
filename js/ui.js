6b8287e19101d65fd93777d981586df9877f0107
Veniamin Zolotukhin <venyaz@gmail.com>
2016-08-27 11:09:27 +0300

    Added HTML5 video player

--- js/ui.js
+++ js/ui.js
@@ -4,4 +4,11 @@
 // Set handlers for .switch
 $('.switch input').click( function(e){
 	$(this).parent('.switch').toggleClass('switch_on');
-})
+});
+
+// Video player
+$('.player__controls').click(function(e) {
+	var video = $(this).parent('.player').find('video').get(0);
+	console.log(video);
+	video.paused? video.play() : video.pause();
+});
